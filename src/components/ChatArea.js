11ff2cfb772496d7e6e6019194bc39140f324a8b
Tolap <nak-online@hotmail.com>
2020-03-21 02:06:56 +0700

    finish add page ui

--- src/components/ChatArea.js
+++ src/components/ChatArea.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './ChatArea.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 class ChatArea extends React.Component {
     constructor(props){
@@ -12,6 +14,36 @@ class ChatArea extends React.Component {
                 <div className="top-bar">
                     <span className="top-bar-header-text">Miniproject</span><br />
                     <span className="top-bar-sub-text">number of members: 5</span>
+                </div>
+                <div className="middle-screen">
+                     <div className="chat-item">
+                         <img className="profile-image chat-item-profile-image" src={require("../images/dog2.png")} />
+                         <div className="chat-item-message">สวัสดี สิริ!</div>
+                     </div>
+                     <div className="chat-item">
+                         <img className="profile-image chat-item-profile-image" src={require("../images/dog3.png")} />
+                         <div className="chat-item-message">สวัสดี มีไรให้ช่วยหรอมานุดดด!</div>
+                     </div>
+                     <div className="chat-item">
+                         <img className="profile-image chat-item-profile-image" src={require("../images/dog2.png")} />
+                         <div className="chat-item-message">ช่วยทำโปรเจคหน่อยจิ</div>
+                     </div>
+                     <div className="chat-item-me">
+                         <div className="chat-item-message">ทำไรกันอยู่หนะพวกนายยยยยย</div>
+                     </div>
+                     <div className="chat-item">
+                         <img className="profile-image chat-item-profile-image" src={require("../images/dog2.png")} />
+                         <div className="chat-item-message">มีโปรเจคเยอะเลยไม่รู้อาจารย์จะสั่งอะไรนักหนาาาา!</div>
+                     </div>
+                     <div className="chat-item">
+                         <img className="profile-image chat-item-profile-image" src={require("../images/dog4.png")} />
+                         <div className="chat-item-message">พวกนายนินทาอาจารย์อยู่ใช่มั๊ยยยยยย</div>
+                     </div>
+                </div>
+                <div className="bottom-input">
+                     <img className="profile-image" src={require("../images/dog1.png")} />
+                     <input className="chat-input" type="text" placeholder="enter your message..." />
+                     <button className="send-button"><FontAwesomeIcon icon={faPaperPlane} style={{marginRight: "5px"}} />Send</button>
                 </div>
             </div>
         );
